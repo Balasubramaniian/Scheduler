2e699ae74dfef890e383ecdf15ff86840b9f618f
balasubrmaniyan.s <balasubramaniyan.sel@gmail.com>
2025-12-16 23:28:51 +0530

    changes

--- src/main/java/com/example/demo/SchdulerApplication.java
+++ src/main/java/com/example/demo/SchdulerApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchdulerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchdulerApplication.class, args);
 	}
+	
+	
+	@Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
